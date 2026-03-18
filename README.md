a6ebeab5b1e6e908321f8e92eebeb697754c4bf7
Laura <spadalaura372@gmail.com>
2026-03-18 16:40:34 -0300

    nova fórmula

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Business Intelligence - Trabalho
 
+### Campos Utilizados
 
+- <u>nome e final do cartão</u>  
+importante para identificar o usuário
 
+- <u>data da compra e data da parcela</u>  
+importante já que elas irão dar a partida na análise dos dados, sendo separadas em seus respectivos períodos para análise  
+
+- <u>categoria e descrição</u>  
+ao separar os gastos por categoria fica mais fácil compreender os dados conforme as prioridades e necessidades do usuário
+
+- <u>valor</u>  
+o principal campo da análise de dados, já que com ele serão feitos os cálculos dos gastos especificados
+
+### <u>Perguntas de Negócio</u>  
+> quais categorias tiveram mais gastos por mês/bimestre/semestre  
+> quais categorias tiveram menos gastos por mês/bimestre/semestre  
+> evolução mensal dos gastos  
+> rank de meses com mais gastos  
+> rank de meses que mais tiveram gastos comparados com as categorias  
+> rank dos anos que mais tiveram gastos  
+> contagem de compras parceladas e compras à vista  
+
+<u>Situação Atual</u>  
+	hoje em dia o usuário pega o extrato da conta e analisa manualmente os gastos separando eles por categoria, parcelas, data de compra
+
+<u>Benefícios de Aplicar BI</u>  
+com o BI aplicado ficaria melhor a visualização de gastos conforme os campos especificados do usuário, para melhor compreensão visual dos gastos.
+
+	
+```
 
 ## Modelo Conceitual
 
--- etl_papeline.py
+++ etl_papeline.py
@@ -1,88 +1,24 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import glob
 
-# -----------------------------
-# CONFIGURAÇÕES
-# -----------------------------
+#df_faturas = pd.read_csv("data/Fatura_2026-01-20.csv")
 
-# Caminho do arquivo CSV exportado do Excel
-CSV_FILE = "data/Fatura_2026-01-20.csv"
+arquivos = glob.glob("data/*.csv")
 
-# String de conexão do banco (MySQL)
-DB_CONNECTION = "mysql+pymysql://usuario:senha@localhost:3306/datawarehouse"
+dfs = [pd.read_csv(arquivo) for arquivo in arquivos]
 
-# Nome da tabela destino no Data Warehouse
-TABLE_NAME = "fato_vendas"
+df_faturas = pd.concat(dfs)
 
-# -----------------------------
-# EXTRACT
-# -----------------------------
-def extract():
-    print("Extraindo dados do CSV...")
+#print(df_faturas)
 
-    df = pd.read_csv(CSV_FILE, encoding="utf-8", sep=",")
+# Ordenar os dados pela Categoria
+print(df_faturas.sort_values(by='Categoria', ascending=True))
 
-    return df
+# Mostrar a quantidade de cada Categoria
+print(df_faturas['Categoria'].value_counts())
 
-# -----------------------------
-# TRANSFORM
-# -----------------------------
-def transform(df):
-    print("Transformando dados...")
+# Mostrar a quantidade transações por usuário
+print(df_faturas['Nome no Cartão'].value_counts())
 
-    # Padronizar nomes das colunas
-    df.columns = (
-        df.columns
-        .str.strip()
-        .str.lower()
-        .str.replace(" ", "_")
-    )
-
-    # Remover linhas totalmente vazias
-    df = df.dropna(how="all")
-
-    # Exemplo de conversões
-    if "data" in df.columns:
-        df["data"] = pd.to_datetime(df["data"], errors="coerce")
-
-    if "valor" in df.columns:
-        df["valor"] = (
-            df["valor"]
-            .astype(str)
-            .str.replace(",", ".")
-            .astype(float)
-        )
-
-    # Remover duplicados
-    df = df.drop_duplicates()
-
-    return df
-
-# -----------------------------
-# LOAD
-# -----------------------------
-def load(df):
-    print("Carregando dados no Data Warehouse...")
-
-    engine = create_engine(DB_CONNECTION)
-
-    df.to_sql(
-        TABLE_NAME,
-        engine,
-        if_exists="append",   # append / replace
-        index=False
-    )
-
-    print("Carga finalizada com sucesso!")
-
-# -----------------------------
-# PIPELINE ETL
-# -----------------------------
-def run_etl():
-    df = extract()
-    df_transformado = transform(df)
-    load(df_transformado)
-
-
-if __name__ == "__main__":
-    run_etl()
+# Mostrar a quantidade de transações por data
+print(df_faturas['Data de Compra'].value_counts())
